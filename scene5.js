aa17c00e5819ce2e9342865bdbe5e3912fb0ea98
ideat <ideat@ideats-MacBook-Pro.local>
2023-07-24 06:26:09 +0700

    update scene5

--- scene5.js
+++ scene5.js
@@ -16,9 +16,31 @@ tl5.fromTo(
     opacity: 0,
   },
   {
-    y: 50,
+    y: 140,
     opacity: 1,
     duration: 3,
+  }
+);
+
+tl5.fromTo(
+  "#text5-1",
+  {
+    opacity: 0,
+  },
+  {
+    opacity: 1,
+    duration: 1,
+  }
+);
+
+tl5.fromTo(
+  "#text5-1",
+  {
+    opacity: 1,
+  },
+  {
+    opacity: 0,
+    duration: 1,
   }
 );
 
@@ -29,7 +51,18 @@ tl5.fromTo(
   },
   {
     opacity: 0,
-    duration: 3,
+    duration: 1,
+  }
+);
+
+tl5.fromTo(
+  "#text5-2",
+  {
+    opacity: 0,
+  },
+  {
+    opacity: 1,
+    duration: 1,
   }
 );
 
@@ -39,8 +72,43 @@ tl5.fromTo(
     opacity: 0,
   },
   {
-    y: 50,
+    y: 140,
     opacity: 1,
+    duration: 3,
+  },
+  "<+=0.5"
+);
+
+tl5.fromTo(
+  "#plastic5-1",
+  {
+    opacity: 1,
+  },
+  {
+    rotate: 45,
+    duration: 1,
+  }
+);
+
+tl5.fromTo(
+  "#text5-2",
+  {
+    opacity: 1,
+  },
+  {
+    opacity: 0,
+    duration: 1,
+  }
+);
+
+tl5.fromTo(
+  "#plastic5-1",
+  {
+    opacity: 1,
+  },
+  {
+    y: 200,
+    opacity: 0,
     duration: 3,
   }
 );
