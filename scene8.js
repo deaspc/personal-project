3effb4475e7c19518ca4d9f59e69c6b288bd712b
ideat <ideat@ideats-MacBook-Pro.local>
2023-07-27 06:19:18 +0700

    scene8  update

--- scene8.js
+++ scene8.js
@@ -4,12 +4,12 @@ const tl8 = gsap.timeline({
     trigger: ".container8",
     pin: true, // pin the trigger element while active
     start: "top top", // when the top of the trigger hits the top of the viewport
-    end: "bottom bottom", // end after scrolling 500px beyond the start
+    end: "max", // end after scrolling 500px beyond the start
     scrub: true, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
     pinSpacing: false,
-    onLeave: function () {
-      window.scrollTo(0, 0);
-    },
+    // onLeave: function () {
+    //   window.scrollTo(0, 0);
+    // },
   },
 });
 
@@ -22,4 +22,32 @@ tl8.fromTo(
     opacity: 0,
     duration: 1,
   }
+);
+
+tl8.fromTo(
+  "#microplastic1",
+  {
+    y: -100,
+    opacity: 0,
+  },
+  {
+    y: 0,
+    opacity: 1,
+    duration: 1,
+  },
+  "<+=0.2"
+);
+
+tl8.fromTo(
+  "#text8-1",
+  {
+    y: -10,
+    opacity: 0,
+  },
+  {
+    y: 0,
+    opacity: 1,
+    duration: 1,
+  },
+  "<+=0.2"
 );
